bdde8825d257787f6fcee31640ba671831ab3430
mrpalui <85637281+mrpalui@users.noreply.github.com>
2025-12-07 05:28:15 +0700

    Update title in layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Dramabox Mobile",
+  title: "Dramabox Mobile by Mr-PaLui",
   description: "Stream unlimited drama.",
   viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0",
 }
